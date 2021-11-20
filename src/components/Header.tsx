665e7413b1e5c13347e53e68267b2c94f450c6b3
Oleksandr Andriichuk <39922731+Dru1DD@users.noreply.github.com>
2021-11-20 14:37:57 +0100

    last commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/Header.css"
 import navbarBack from '../assets/TopBarWithoutText.png'
+import WebFont from "webfontloader";
+import { useHistory } from 'react-router'
+import unispace_bd from '../assets/fonts/unispace bd.ttf'
 
 interface HeaderProps {
   isModal: boolean
@@ -9,6 +12,32 @@ interface HeaderProps {
 
 const Header: React.FunctionComponent<HeaderProps> = ({isModal, setIsModal}) => {
 
+  let history = useHistory()
+
+  useEffect(() => {
+    try {
+      WebFont.load({
+      custom: {
+        families: ['Unispace-Bold'],
+        urls: [`${unispace_bd}`]
+      }
+    })
+    } catch(e) {
+      console.log(e)
+    }
+    
+  }, [])
+  const pageHandler: (num: number) => void = (num) => {
+    switch(num) {
+      case 0: 
+        return history.push('/marketplace')
+      case 1:
+        return history.push('/gallery')
+      case 2: 
+        return history.push('/mystuff')
+    }
+  }
+
   return (
     <nav className="navbar_container">
       <img className="navbar_back" src={navbarBack} alt="navbar_back" />
@@ -16,19 +45,20 @@ const Header: React.FunctionComponent<HeaderProps> = ({isModal, setIsModal}) => 
  <ul
         className="navbar__list"
       >
-            <li className="navbar__item" >
-              <p className="navbar__link">MARKETPLACE</p>
+            <li className="navbar__item" onClick={() => pageHandler(0)}>
+              <p className="navbar__link" style={{ fontFamily: 'Unispace-Bold'}}>MARKETPLACE</p>
             </li>
-            <li className="navbar__item" >
-              <p className="navbar__link">GALLERY</p>
+            <li className="navbar__item" onClick={() => pageHandler(1)}>
+              <p className="navbar__link" style={{ fontFamily: 'Unispace-Bold'}}>GALLERY</p>
             </li>
-            <li className="navbar__item" >
-              <p className="navbar__link">MY STUFF</p>
+            <li className="navbar__item" onClick={() => pageHandler(2)}>
+              <p className="navbar__link" style={{ fontFamily: 'Unispace-Bold'}} >MY STUFF</p>
             </li>
             <li className="navbar__item" >
               <p 
                 className="navbar_btn"
                 onClick={() => setIsModal(!isModal)}
+                style={{ fontFamily: 'Unispace-Bold'}}
               >CONNECT WALLET</p>
             </li>
       </ul>
